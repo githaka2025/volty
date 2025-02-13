ccead0f11a54c2be93884466d8f00c950a64f36e
githaka2025 <githaka2025@gmail.com>
2025-02-13 15:17:43 +0300

    refactor(ui/layout): mount Navigation component in Header component

--- src/ui/layout/Header.tsx
+++ src/ui/layout/Header.tsx
@@ -1,10 +1,14 @@
 import { HeaderStyles } from '@layoutStyles';
+import { Navigation } from '@layoutUI';
 import { Brand } from '@sharedUI';
 
 export default function Header() {
   return (
     <header className={[HeaderStyles.Header, 'layout'].join(' ')}>
       <Brand name='Volty' />
+      <Navigation>
+        <></>
+      </Navigation>
     </header>
   );
 }
