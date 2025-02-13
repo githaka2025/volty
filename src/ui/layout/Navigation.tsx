84de9cc075d0078ef1930737191dad10e81dc734
githaka2025 <githaka2025@gmail.com>
2025-02-13 16:53:24 +0300

    feat(ui/layout): add toggle functionality in Navigation

--- src/ui/layout/Navigation.tsx
+++ src/ui/layout/Navigation.tsx
@@ -1,21 +1,31 @@
+'use client';
+import { useToggle } from '@hooks';
 import { NavigationStyles } from '@layoutStyles';
 import { Icon } from '@sharedUI';
 import { UI } from '@types';
 
 export default function Navigation({ children }: UI.Layout.Navigation.Props) {
+  const { state, toggleState } = useToggle();
   return (
     <nav className={NavigationStyles.Navigation}>
       <div className={NavigationStyles.Large}>{children}</div>
       <div className={NavigationStyles.Small}>
-        <div className={NavigationStyles.Head}>
+        <div
+          className={NavigationStyles.Head}
+          onClick={toggleState}
+        >
           <Icon
-            name='cancel.svg'
+            name={state ? 'cancel.svg' : 'menu.svg'}
             alt='Menu icon.'
             width={24}
             height={12}
           />
         </div>
-        <div className={NavigationStyles.Body}>{children}</div>
+        {state ? (
+          <div className={NavigationStyles.Body}>{children}</div>
+        ) : (
+          <></>
+        )}
       </div>
     </nav>
   );
