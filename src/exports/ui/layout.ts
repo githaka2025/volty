78f7027561be35b2d0bc6fc5047eee45608b622a
githaka2025 <githaka2025@gmail.com>
2025-02-13 11:25:14 +0300

    feat(components/layout): create and export Main component and its style module

--- src/exports/ui/layout.ts
+++ src/exports/ui/layout.ts
@@ -1,1 +1,2 @@
 export { default as Header } from '@/ui/layout/Header';
+export { default as Main } from '@/ui/layout/Main';
---
+++ src/ui/layout/Main.tsx
@@ -0,0 +1,5 @@
+import { MainStyles } from '@layoutStyles';
+
+export default function Main() {
+  return <main className={MainStyles.Main}></main>;
+}
