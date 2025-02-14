dfcdc1b8a8fa7aeafdcb8732e1f43b928e64f7dc
githaka2025 <githaka2025@gmail.com>
2025-02-14 12:37:42 +0300

    feat(components/views): display current pathname in PageNotFound component

--- src/components/views/PageNotFound.tsx
+++ src/components/views/PageNotFound.tsx
@@ -1,8 +1,11 @@
+'use client';
 import { View } from '@layoutComponents';
 import { Button, Logo } from '@sharedComponents';
 import { PageNotFoundStyles } from '@viewsStyles';
+import { usePathname } from 'next/navigation';
 
 export default function PageNotFound() {
+  const pathname = usePathname();
   return (
     <View
       id='error-404'
@@ -11,6 +14,7 @@ export default function PageNotFound() {
       <Logo />
       <div className={PageNotFoundStyles.Body}>
         <h1>404 - Page Not Found</h1>
+        <p>{pathname}</p>
         <p>The requested URL could not be located on this server.</p>
         <Button
           href=''
