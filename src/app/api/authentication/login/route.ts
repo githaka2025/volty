599ef9f9af76d1c5d256be4ebaf71100764c4e5e
githaka2025 <githaka2025@gmail.com>
2025-02-15 10:42:47 +0300

    feat(api/authentication): add login logic - verifyLogin

--- src/app/api/authentication/login/route.ts
+++ src/app/api/authentication/login/route.ts
@@ -1,3 +1,32 @@
-export async function POST() {
-  return Response.json({ success: true });
+import { Handlers } from '@types';
+import { NextRequest, NextResponse } from 'next/server';
+import { verifyLogin } from '@handlers';
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    const isLoginOkay = verifyLogin({ email, password });
+    if (!isLoginOkay.success) {
+      return NextResponse.json(isLoginOkay, {
+        status: 400,
+      });
+    }
+  } catch {
+    return Response.json(
+      {
+        success: false,
+        error: {
+          type: 'handlers',
+          origin: '/api/authentication/login',
+          message:
+            'An error occurred while trying to log in. Please try again.',
+        },
+      } as Handlers.Response,
+      {
+        status: 500,
+      }
+    );
+  }
+  return Response.json({ success: true } as Handlers.Response, { status: 200 });
 }
