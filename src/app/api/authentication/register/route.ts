3c8353b49a78a3fbda9eb2c204ca7fab5e634b38
githaka2025 <githaka2025@gmail.com>
2025-02-15 11:17:49 +0300

    feat(api/authentication): add register logic - verifyRegister

--- src/app/api/authentication/register/route.ts
+++ src/app/api/authentication/register/route.ts
@@ -1,3 +1,32 @@
-export async function POST() {
-  return Response.json({ success: true });
+import { Handlers } from '@types';
+import { NextRequest, NextResponse } from 'next/server';
+import { verifyRegister } from '@handlers';
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password, confirmPassword } = await request.json();
+
+    const isRegisterOkay = verifyRegister({ email, password, confirmPassword });
+    if (!isRegisterOkay.success) {
+      return NextResponse.json(isRegisterOkay, {
+        status: 400,
+      });
+    }
+  } catch {
+    return Response.json(
+      {
+        success: false,
+        error: {
+          type: 'handlers',
+          origin: '/api/authentication/register',
+          message:
+            'An error occurred while trying to register. Please try again.',
+        },
+      } as Handlers.Response,
+      {
+        status: 500,
+      }
+    );
+  }
+  return Response.json({ success: true } as Handlers.Response, { status: 200 });
 }
